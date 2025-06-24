4a0382525f29e394a05d1fd3abc037c014015c93
VidadiJavadov <vidadi4000@gmail.com>
2025-06-25 03:40:05 +0400

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""This Square class code"""
+
+
+class Square:
+    """in this class we will get size"""
+
+    def __init__(self, size=0):
+        """checking and getting the variable - size"""
+
+        if not isinstance(size, int):
+
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = size
+    
+    def size(self):
+        """This is getter"""
+
+        return self.__size
+
+    def size(self, value):
+        """this is setter"""
+        
+        self.__size = value
+
+    def area(self):
+        """This a function wchich returns area of square"""
+
+        return self.__size ** 2
