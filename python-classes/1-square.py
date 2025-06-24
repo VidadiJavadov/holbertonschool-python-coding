dd08257e42f87296e5a19034fe132d23f1b4dd81
VidadiJavadov <vidadi4000@gmail.com>
2025-06-25 02:52:48 +0400

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,9 +9,9 @@ class Square:
     def __init__(self, size=0):
         """checking and getting the variable - size"""
 
-        self.__size = size
-
         if not isinstance(int, size):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if size < 0: 
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
