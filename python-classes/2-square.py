055583841a22cea2e0c6588f8d394a0253616edf
VidadiJavadov <vidadi4000@gmail.com>
2025-06-25 03:36:21 +0400

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -18,8 +18,10 @@ class Square:
             raise ValueError("size must be >= 0")
 
         else:
-            self.__size = size
             
+            self.__size = size
+
+    
     def area(self):
         """This a function wchich returns area of square"""
 
