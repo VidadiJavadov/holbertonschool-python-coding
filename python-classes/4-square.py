c1e49eab9104086f9c3734b7cf3289f01fa1887a
VidadiJavadov <vidadi4000@gmail.com>
2025-06-25 04:02:47 +0400

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""This Square class code"""
+
+
+class Square:
+    """in this class we will get size"""
+
+    def __init__(self, size=0):
+        """checking and getting the variable - size"""
+
+        if not isinstance(size, int):
+
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = size
+    
+    @property
+    def size(self):
+        """This is getter"""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """this is setter"""
+        if not isinstance(value, int):
+
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = value
+
+    def area(self):
+        """This a function wchich returns area of square"""
+
+        return self.__size ** 2
+    
+    def my_print(self):
+        """This is a function that prints square with # symbols"""
+        if self.__size == 0:
+             
+            print()
+            
+        else:
+            for i in range(self.__size):
+                print(self.__size * "#")
